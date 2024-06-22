234014ecf71d511dd128ff4c0d25248b28d3aba8
Nataliya Yakovyna <yakovina.n13@gmail.com>
2024-06-22 18:45:21 +0300

    main

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,62 @@
+export async function getImages(request) {
+  const BASE_URL = 'https://pixabay.com';
+  const END_POINT = '/api/';
+  const params = new URLSearchParams({
+    key: '44418953-cdbfe82a1d191a3eebe28ee3b',
+    q: request,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: 1,
+    per_page: 100,
+  });
+
+  const url = `${BASE_URL}${END_POINT}?${params}`;
+  console.log(url);
+
+  const options = {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content - Type': 'application/json',
+
+      'X-RateLimit-Limit': '100',
+      'X-RateLimit-Remaining': '99',
+      'X-RateLimit-Reset': '0.6',
+    },
+  };
+  const res = await fetch(url, { options });
+  if (res.ok) {
+    return res.json();
+  } else {
+    throw new Error(res.status);
+  }
+}
+
+// getImages(`dog`);
+
+// async function getImages(request) {
+//     const BASE_URL = 'https://pixabay.com';
+//     const END_POINT = '/api/';
+//     const params = new URLSearchParams({
+//         key: '44418953-cdbfe82a1d191a3eebe28ee3b',
+//         q: `${request}`,
+//         image_type: 'photo',
+//         orientation: 'horizontal',
+//         safesearch: true,
+//     });
+
+//     const url = `${BASE_URL}${END_POINT}?${params}`;
+//     console.log(url);
+
+//     const headers = {
+//         'Content-Type': 'application/json',
+
+//         'X-RateLimit-Limit': '100',
+//         'X-RateLimit-Remaining': '99',
+//         'X-RateLimit-Reset': '0.6',
+//     };
+
+//     const res = await fetch(url, { headers });
+//     return await res.json();
+
+// };
--- src/main.js
+++ src/main.js
@@ -1,2 +1,64 @@
 import './js/pixabay-api';
 import './js/render-function';
+
+// console.log(btnEl);
+import { getImages } from './js/pixabay-api';
+import { imagesTemplate } from './js/render-function';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import imageUrl from './img/alert-icon.svg';
+// import listEl from './js/render-function'
+
+const formEl = document.querySelector('.form');
+const inputEl = document.querySelector('.form-input');
+const imagesList = document.querySelector('.gallery');
+const loaderEl = document.querySelector('.loader');
+
+// loaderEl.style.display = 'none';
+
+formEl.addEventListener('submit', e => {
+  e.preventDefault();
+  imagesList.innerHTML = '';
+  // loaderEl.style.display = 'inline-block';
+  loaderEl.classList.toggle('.visually-hidden');
+
+  const request = e.target.elements.photo.value.trim();
+  //   console.log(request);
+
+  if (request === '') {
+    showIziToast();
+  } else {
+    getImages(request)
+      .then(data => {
+        inputEl.value = '';
+        //   loaderEl.style.display = 'none';
+        if (data.total === 0) {
+          showIziToast();
+        } else {
+          return imagesTemplate(data.hits);
+        }
+      })
+      .catch(err => alert(err))
+      .finally(() => loaderEl.classList.toggle('.visually-hidden'));
+  }
+});
+
+function showIziToast() {
+  return iziToast.show({
+    message:
+      'Sorry, there are no images matching your search query. Please try again!',
+    messageColor: '#fff',
+    messageSize: '16',
+    messageLineHeight: '0.03em',
+    titleColor: '#fff',
+    titleSize: '16',
+    titleLineHeight: '0.03em',
+    position: 'topRight',
+    backgroundColor: '#ef4040',
+    theme: 'dark',
+    iconUrl: imageUrl,
+    iconColor: '#FAFAFB',
+  });
+}
